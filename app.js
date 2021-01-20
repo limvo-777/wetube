9eab6790d9e428be5b3f788c4fbc7986bffd3655
Dongmin Kim <lejpf54@gmail.com>
2021-01-21 00:09:02 +0900

    Add Routing

--- app.js
+++ app.js
@@ -3,20 +3,13 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { userRouter } from "./router";
 
 const app = express();
-const PORT = 4000;
-
-const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
 const handleHome = (req,res) => res.send("Hello from ya!!");
 
 const handleProfile = (req,res) => res.send("You are on my profile");
-
-// const betweenHome = (req,res,next) => {
-//     console.log("Middleware example");
-//     next();
-// };
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -28,4 +21,6 @@ app.get("/", handleHome);
 
 app.get("/profile",handleProfile);
 
-app.listen(PORT, handleListening);
+app.use("/user", userRouter);
+
+export default app;
