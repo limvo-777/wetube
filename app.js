009122f3d45caa4716827542ca8aa0c2a57a0de4
Dongmin Kim <lejpf54@gmail.com>
2021-01-27 02:28:29 +0900

    pug template and use local variable

--- app.js
+++ app.js
@@ -3,24 +3,25 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
+import { localMiddlewares } from "./middlewares"
+import routes from "./routes";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 
 const app = express();
 
-const handleHome = (req,res) => res.send("Hello from ya!!");
-
-const handleProfile = (req,res) => res.send("You are on my profile");
-
+app.use(helmet());
+app.set("view engine","pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(helmet());
 app.use(morgan("dev"));
+app.use(localMiddlewares);
 
-app.get("/", handleHome);   
-
-app.get("/profile",handleProfile);
-
-app.use("/user", userRouter);
+app.use(routes.home, globalRouter);
+// app.get(routes.users, (req, res) => res.send("User"));
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
