513e782764730eb95a2f27218a93d28fc2b24c5c
Dongmin Kim <lejpf54@gmail.com>
2021-02-02 00:22:43 +0900

    Post upload and Creating Video part 1

---
+++ models/Comment.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const CommentSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        required: "Text is required"
+    },
+    createdAt:{
+        type:Date,
+        default: Date.now
+    }
+    // ,
+    // video: {
+    //     type: mongoose.SchemaTypes.ObjectId,
+    //     ref: "Video"
+    // }
+})
+
+const model = mongoose.model("Comment",CommentSchema);
+export default model;
